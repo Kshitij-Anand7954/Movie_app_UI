15f865e761cd98255a85c228fdfffb9a8e081eb2
Kshitij Anand <kshitijkumar2002@gmail.com>
2024-06-02 17:17:46 +0530

    added tab section of respective movie

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:movies_app/widgets/ironman_section.dart';
+import 'package:movies_app/widgets/movie_section.dart';
+import 'package:movies_app/widgets/spiderman_section.dart';
+import 'package:movies_app/widgets/thor_section.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -56,9 +60,62 @@ _handelTabSection(){
               fit: BoxFit.cover,
               )
               ),
-            )
+            ),
+            Container(
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(8),
+              image: DecorationImage(image: AssetImage("images/thor3.jpg"),
+              fit: BoxFit.cover,
+              )
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(8),
+              image: DecorationImage(image: AssetImage("images/spiderman3.jpg"),
+              fit: BoxFit.cover,
+              )
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(8),
+              image: DecorationImage(image: AssetImage("images/ironman3.jpg"),
+              fit: BoxFit.cover,
+              )
+              ),
+            ),
           ],
-          )
+          ),
+          SizedBox(height: 30),
+          TabBar(
+            controller: _tabController,
+            unselectedLabelColor: Colors.white.withOpacity(0.5),
+            isScrollable: true,
+            labelStyle: TextStyle(
+
+              color: Colors.white,
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+
+            ),
+
+            dividerHeight: 0,
+            indicatorColor: Colors.redAccent,
+            tabs: [
+              Tab(text:"All"),
+              Tab(text:"Thor"),
+              Tab(text:"Spider Man"),
+              Tab(text:"Iron Man"),
+            ],
+
+          ),
+          SizedBox(height: 20),
+          Center(
+            child: [
+              MoviesSection(),
+              thorSection(),
+              spidermanSection(),
+              IronmanSection(),
+            ][_tabController.index],
+          ),
         ],
       ),
     );
---
+++ lib/widgets/spiderman_section.dart
@@ -0,0 +1,111 @@
+import 'package:carousel_slider/carousel_options.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+class spidermanSection extends StatelessWidget {
+List names=[
+  "Spider Man 3",
+  "Spider Man 2",
+  "Spider Man",
+ 
+
+];
+List images=[
+
+ "images/spiderman3.jpg",
+ "images/spiderman2.jpg",
+ "images/spiderman1.jpg",
+  
+];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CarouselSlider( options: CarouselOptions(
+            autoPlay: true,
+            height: 500,
+            enlargeCenterPage: true,
+            aspectRatio: 16/9,
+            enableInfiniteScroll: true,
+            enlargeStrategy: CenterPageEnlargeStrategy.height,
+            autoPlayAnimationDuration: Duration(milliseconds: 800),
+            autoPlayCurve: Curves.fastOutSlowIn,
+            viewportFraction: 0.65,
+          ),
+          items: [
+           for(int i=0;i<images.length;i++)
+            InkWell(
+              onTap: (){},
+              child: Container(
+                margin: EdgeInsets.all(6),
+                child: Column(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.asset(images[i],
+                      height: 300,
+                      width: 300,
+                      fit: BoxFit.cover,
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.all(8),
+                    
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                          Text("PG-13",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+
+                          ),
+                          ),
+                           Row(
+                            children: [
+                              Icon(Icons.star,color: Colors.amber,),
+                              SizedBox(width: 5,),
+                              Text("4.8",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                
+                              ),
+                              )
+                            ],
+                          ),
+                          Text("Action",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+
+                          ),
+                          ),
+                         
+                      ],
+                    )
+                    ),
+                    SizedBox(height: 5),
+                    Text(names[i],
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+
+
+                    ),
+                    ),
+                    
+                  ],
+                ),
+              ),
+            )
+          ],
+          ),
+      ],
+    );
+  }
+}
